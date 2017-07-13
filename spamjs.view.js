0840f7191a0b7859834f09a966cab29c6442c602
LNT <lalit.tanwar07@gmail.com>
2017-07-13 14:09:49 +0530

    this-self confusion removed

--- spamjs.view.js
+++ spamjs.view.js
@@ -339,7 +339,7 @@ define("spamjs.view").as(function(view) {
                             try {
                                 self.$$.detach();
                                 dff.resolve(self);
-                                __$trash__.append(self.$$);
+                                //__$trash__.append(this.$$);
                             } catch (e) {
                                 console.warn("ViewDetachException:", e)
                             }
